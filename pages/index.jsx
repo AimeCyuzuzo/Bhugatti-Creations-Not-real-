9fb6cfaf002332d0ef56ed41c3a1b981a9aac56c
AimeCyuzuzo <89910012+AimeCyuzuzo@users.noreply.github.com>
2022-04-20 23:39:56 +0200

    Another commit

--- pages/index.jsx
+++ pages/index.jsx
@@ -40,7 +40,7 @@ export default function Home() {
               <p>
                 All this is done by encouraging the youth to advise each other and work in groups in order to achieve something big.
               </p>
-              <p className='goToMainAbout'><Link href="/about"><a>Learn more</a></Link></p>
+              <p className='goToMainAbout'><Link href="/about"><a style={{color:"white"}}>Learn more</a></Link></p>
             </section>
             <section className='aboutSectionTwo'>
               <img src="/heartofgratitude/IMG_3885.JPG" alt="" />
@@ -51,20 +51,32 @@ export default function Home() {
               <h3><i>Who are Isomo Scholars?</i></h3>
               <p>Isomo scholars are the students who attended <strong>Isomo Academy</strong></p>
               <p>
-                <strong>Isomo Academy</strong> is a program that helps well-performing students in senior four or level three(RTB) in all 
-                combinations each year, and teaches them <span className="red">use the content that is in the official b2r website about what happens
-                in Isomo Academy</span>
+                <strong>Isomo Academy</strong> is an intensive English training and college prep program designed to equip Rwanda&apos;s top high school students with the
+                 skills they need to compete for university scholarships in and outside Rwanda.
                 </p>
                 <p>
-                  <i>If you are a senior four or level three students, <a href="#" style={{color:"red"}}>click here</a> to check if the application is 
-                  open, and apply for this amazing program program.</i>
+                  <span style={{fontWeight:"bolder"}}>Note:</span> <i>~If you are a senior four or level three students, <a href="#" style={{textDecoration:"underline"}}>click here</a> to check if the application is 
+                  open, and apply for this amazing program program~</i>
                 </p>
-                <a className="blue" href="#" rel="noreferrer" target="_blank"><i style={{textDecoration:"underline"}}>Learn more</i></a>
-                <p className="note">Isomo Academy is a program managed by <a href="https://www.bridge2rwanda.org" rel="noreferrer" target="_blank" style={{textDecoration:"underline"}} className="blue">Bridge2Rwanda</a></p>
+                <p><a href="https://www.bridge2rwanda.org/isomo-academy/" rel='noreferrer' style={{textDecoration:"underline"}}>Learn more</a></p>
+                <p className="note">Isomo Academy is a program managed by <a href="https://www.bridge2rwanda.org" rel="noreferrer" target="_blank" style={{textDecoration:"underline"}}>Bridge2Rwanda</a></p>
             </div>
             <div className="singleQuestion">
               <h3><i>What is Bridge2Rwanda?</i></h3>
-              <p className="red">Use content from the official bridge2rwanda page</p>
+              <p>
+                <span className="bold">Isomo Academy</span> The program of Isomo Academy is managed by B2R
+              </p>
+              <p>
+                <span className="bold">B2R Scholars</span> A program that helps prepare brightest high school students from Rwanda and neightboring
+                countries to compete and win international scholarships in the best universties around the world.
+              </p>
+              <p>
+                <span className="bold">B2R Career Services</span> Rwanda's premier talent placement provider committed to helping high capacity talent connect with the top employers and career opportunities in Rwanda and East Africa.
+              </p>
+              <p>
+                <span className="bold">B2R Farms</span> A program that provides training and career opportunities for talented university graduates
+              </p>
+              <p><a href="htps://www.bridge2rwanda.org" rel='noreferrer' target="_blank" style={{textDecoration:"underline"}}>Learn more</a></p>
             </div>
               
           </div>
@@ -82,7 +94,7 @@ export default function Home() {
                 <h4 className="adminName">Happy Herman Niyorurema</h4>
                 <p><i>Managing Director</i></p>
                 <div>
-                  <small><a href="mailto:aimecyuzuzo5@gmail.com" rel="noreferrer" target="_blank"><i>niyorurema1@gmail.com</i></a></small>
+                  <small><a href="mailto:niyorurema1@gmail.com" rel="noreferrer" target="_blank"><i>niyorurema1@gmail.com</i></a></small>
                   <small>Rwanda - +250789143105</small>
                 </div>
 
@@ -98,7 +110,7 @@ export default function Home() {
                 <h4 className="adminName">Mutuyimana Dadyine</h4>
                 <p><i>Chairperson</i></p>
                 <div>
-                  <small><a href="mailto:aimecyuzuzo5@gmail.com" rel="noreferrer" target="_blank"><i>isarodadyne@gmail.com</i></a></small>
+                  <small><a href="mailto:isarodadyne@gmail.com" rel="noreferrer" target="_blank"><i>isarodadyne@gmail.com</i></a></small>
                   <small>Rwanda - +25078 <span className="red">Here br0</span></small>
                 </div>
               </section>
@@ -113,7 +125,7 @@ export default function Home() {
                 <h4 className="adminName">Eloi Inema</h4>
                 <p><i>Secretary General</i></p>
                 <div>
-                  <small><a href="mailto:aimecyuzuzo5@gmail.com" rel="noreferrer" target="_blank"><i>eloiinem@gmail.com</i></a></small>
+                  <small><a href="mailto:eloiinema@gmail.com" rel="noreferrer" target="_blank"><i>eloiinem@gmail.com</i></a></small>
                   <small>Rwanda - +250732753600</small>
                 </div>
               </section>
@@ -128,7 +140,7 @@ export default function Home() {
                 <h4 className="adminName">Gikundiro Elvira</h4>
                 <p><i>Finance Officer</i></p>
                 <div>
-                  <small><a href="mailto:aimecyuzuzo5@gmail.com" rel="noreferrer" target="_blank"><i>gikelvira@gmail.com</i></a></small>
+                  <small><a href="mailto:gikelvira@gmail.com" rel="noreferrer" target="_blank"><i>gikelvira@gmail.com</i></a></small>
                   <small>Rwanda - +250780898928</small>
                 </div>
               </section>
@@ -143,7 +155,7 @@ export default function Home() {
                 <h4 className="adminName">Patmos Acher Mpakaniye</h4>
                 <p><i>Public Relations Officer</i></p>
                 <div>
-                  <small><a href="mailto:aimecyuzuzo5@gmail.com" rel="noreferrer" target="_blank"><i>achaire07@gmail.com</i></a></small>
+                  <small><a href="mailto:achaire07@gmail.com" rel="noreferrer" target="_blank"><i>achaire07@gmail.com</i></a></small>
                   <small>Rwanda - +25073866666304</small>
                 </div>
               </section>
@@ -158,7 +170,7 @@ export default function Home() {
                 <h4 className="adminName">Edison Uwamungu</h4>
                 <p><i>IT Manager</i></p>
                 <div>
-                  <small><a href="mailto:aimecyuzuzo5@gmail.com" rel="noreferrer" target="_blank"><i>iamedisonu250@gmail.com</i></a></small>
+                  <small><a href="mailto:iamedisonu250@gmail.com" rel="noreferrer" target="_blank"><i>iamedisonu250@gmail.com</i></a></small>
                   <small>Rwanda - +250785971157</small>
                 </div>
               </section>
@@ -173,7 +185,7 @@ export default function Home() {
                 <h4 className="adminName">Keza Kayihura Herta</h4>
                 <p><i>Projects Manager</i></p>
                 <div>
-                  <small><a href="mailto:aimecyuzuzo5@gmail.com" rel="noreferrer" target="_blank"><i>kezaherta06@gmail.com</i></a></small>
+                  <small><a href="mailto:kezaherta06@gmail.com" rel="noreferrer" target="_blank"><i>kezaherta06@gmail.com</i></a></small>
                   <small>Rwanda - +250790564546</small>
                 </div>
               </section>
@@ -188,7 +200,7 @@ export default function Home() {
                 <h4 className="adminName">Bagabo Allain</h4>
                 <p><i>Assistant Projects Manager</i></p>
                 <div>
-                  <small><a href="mailto:aimecyuzuzo5@gmail.com" rel="noreferrer" target="_blank"><i>kezaherta06@gmail.com</i></a></small>
+                  <small><a href="mailto:" rel="noreferrer" target="_blank"><i className='red'>kezaherta06@gmail.com</i></a></small>
                   <small>Rwanda - +250784104771</small>
                 </div>
               </section>
@@ -199,9 +211,9 @@ export default function Home() {
           <h2 className="strongGreen" style={{
           }}>Latest Events</h2>
           <div className="eventsBoxes">
-            <Link href="/posts/:id" style={{cursor:"pointer"}}>
-              <a>
-                <div className='singleEvent'>
+            <div className='singleEvent'>
+              <Link href="/posts/:id" style={{cursor:"pointer"}}>
+                <a>
                   <h4 className="eventTitle">Visiting Nyanza Memorial Site in Kicukiro</h4>
                   <div className="eventContent">
                     <section className='eventContentOne'>
@@ -216,12 +228,12 @@ export default function Home() {
                       </div>
                     </section>
                   </div>
-                </div>
-              </a>
-            </Link>
-            <Link href="/posts/:id" style={{cursor:"pointer"}}>
-              <a>
-                <div className='singleEvent'>
+                </a>
+              </Link>
+            </div>
+            <div className='singleEvent'>
+              <Link href="/posts/:id" style={{cursor:"pointer"}}>
+                <a>
                   <h4 className="eventTitle">Heart of Gratitude</h4>
                   <div className="eventContent">
                     <section className='eventContentOne'>
@@ -236,9 +248,9 @@ export default function Home() {
                       </div>
                     </section>
                   </div>
-                </div>
-              </a>
-            </Link>
+                </a>
+              </Link>
+            </div>
           </div>
         </div>
         <Footer />
